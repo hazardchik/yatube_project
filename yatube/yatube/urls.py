c33bb7be810647e01c8053f825b72f1f190db1d4
Никита Каширин <kashirin.nikita@yandex.ru>
2022-12-08 17:42:02 +0700

    Namescape and names

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
+    path('', include('posts.urls'), namescape = 'main_posts'),
     path('group/<slug:slug>/', include('posts.urls')),
 ]
